9f789c458ab7e34466dc2493bd4f892093ca7963
LexuantamLe <lexuantamle6@gmail.com>
2024-08-19 22:37:47 +0700

    Update 1.py

--- PART 1/basic type variable/1.py
+++ PART 1/basic type variable/1.py
@@ -1,1 +1,9 @@
-
+st=str(input("Input String:"))
+if len(st)%2 == 0 :
+    for i in range(int(len(st)/2)):
+        if st[i] != st[len(st)-1-i]:
+            print("Not Mirror String")
+            exit()
+    print("Mirror String")
+else:
+    print("Not Mirror String")
